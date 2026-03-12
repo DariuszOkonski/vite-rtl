d8696ccfe7f5229d68262913f8829be292bc5e5d
Dariusz Okoński <darek200180@gmail.com>
2026-03-12 07:24:49 +0100

    add second test

--- src/utils.test.js
+++ src/utils.test.js
@@ -7,5 +7,10 @@ describe('utils', () => {
       const result = range(1, 6);
       expect(result).toEqual([1, 2, 3, 4, 5]);
     });
+
+    it('returns correct result from 41-45 range', () => {
+      const result = range(41, 45);
+      expect(result).toEqual([41, 42, 43, 44]);
+    });
   });
 });
