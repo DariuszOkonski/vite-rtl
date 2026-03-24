6627dc04347b922cadc5aa9b156c075f9a09eef3
Dariusz Okoński <darek200180@gmail.com>
2026-03-24 08:08:17 +0100

    add tests for Pagination

--- src/pagination/Pagination_01.test.jsx
+++ src/pagination/Pagination_01.test.jsx
@@ -1,9 +1,36 @@
-import { render } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 import Pagination from './Pagination';
+import userEvent from '@testing-library/user-event';
 
 describe('Pagination', () => {
   it('renders correct pagination', () => {
     render(<Pagination total={50} limit={10} currentPage={1} />);
+
+    const pageContainers = screen.getAllByTestId('page-container');
+
+    expect(pageContainers.length).toEqual(5);
+    expect(pageContainers).toHaveLength(5);
+    expect(pageContainers[0]).toHaveTextContent('1');
+  });
+
+  it('should emit clicked page', async () => {
+    const user = userEvent.setup();
+    const handleClick = vi.fn();
+
+    render(
+      <Pagination
+        total={50}
+        limit={10}
+        currentPage={1}
+        selectPage={handleClick}
+      />,
+    );
+
+    const pageContainers = screen.getAllByTestId('page-container');
+    await user.click(pageContainers[0]);
+
+    expect(handleClick).toHaveBeenCalledOnce();
+    expect(handleClick).toHaveBeenCalledWith(1);
   });
 });
