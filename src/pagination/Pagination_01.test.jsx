9902528c4863187adddb36afdce85840939abe17
Dariusz Okoński <darek200180@gmail.com>
2026-03-25 07:09:04 +0100

    add tests to Pagination

--- src/pagination/Pagination_01.test.jsx
+++ src/pagination/Pagination_01.test.jsx
@@ -2,6 +2,13 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import Pagination from './Pagination';
 import userEvent from '@testing-library/user-event';
+import * as utils from '../utils';
+
+vi.mock('../utils.js', () => {
+  return {
+    range: () => [1, 2, 3, 4, 5],
+  };
+});
 
 describe('Pagination', () => {
   it('renders correct pagination', () => {
@@ -32,5 +39,12 @@ describe('Pagination', () => {
 
     expect(handleClick).toHaveBeenCalledOnce();
     expect(handleClick).toHaveBeenCalledWith(1);
+  });
+
+  it('spies on utils', () => {
+    vi.spyOn(utils, 'range');
+    render(<Pagination total={50} limit={10} currentPage={1} />);
+
+    expect(utils.range).toHaveBeenCalledWith(1, 6);
   });
 });
