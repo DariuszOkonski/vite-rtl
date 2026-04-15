406480835c504832b7b60ad063578dd09ea7b9ba
Dariusz Okoński <darek200180@gmail.com>
2026-04-15 07:56:39 +0200

    add ErrorMessage_02

---
+++ src/01_errorMessage/ErrorMessage_02.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import ErrorMessage from './ErrorMessage';
+
+describe('ErrorMessage_02', () => {
+  it('renders with default message', () => {
+    render(<ErrorMessage />);
+
+    const messageContainer = screen.getByTestId('message-container');
+
+    expect(messageContainer).toHaveTextContent('Something went wrong');
+  });
+
+  it('renders with custom message', () => {
+    render(<ErrorMessage message='404 Error' />);
+
+    const messageContainer = screen.getByTestId('message-container');
+
+    expect(messageContainer).toHaveTextContent('404 Error');
+  });
+});
