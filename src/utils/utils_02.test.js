0bcfcaaee0d1ab074318f782539875acab2bbd40
Dariusz Okonski <darek200180@gmail.com>
2026-05-19 09:25:37 +0200

    add styles for utils

--- src/utils/utils_02.test.js
+++ src/utils/utils_02.test.js
@@ -1,13 +1,18 @@
 import { describe, expect, it } from "vitest";
 import { range } from "./utils";
 
+describe("utils", () => {
+  describe("range", () => {
+    it("check basic array 1-6", () => {
+      const result = range(1, 6);
 
-describe('utils', () => {
-    describe('range', () => {
-        it('renders proper array', () => {
-            const result = range(1, 6)
+      expect(result).toEqual([1, 2, 3, 4, 5]);
+    });
 
-            expect(result).toEqual([1,2,3,4,5])
-        })
-    })
-})
+    it("check array with range 41-45", () => {
+      const result = range(41, 46);
+
+      expect(result).toEqual([41, 42, 43, 44, 45]);
+    });
+  });
+});
