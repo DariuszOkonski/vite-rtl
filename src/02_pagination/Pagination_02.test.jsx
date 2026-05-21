f0e815e8351f0ed69d6ef4e4a804948e307ef399
Dariusz Okoński <darek200180@gmail.com>
2026-05-21 09:12:38 +0200

    update on pagination_02

--- src/02_pagination/Pagination_02.test.jsx
+++ src/02_pagination/Pagination_02.test.jsx
@@ -2,14 +2,19 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import Pagination from './Pagination';
 import userEvent from '@testing-library/user-event';
+import * as utils from '../utils/utils';
+
+vi.mock('../utils/utils.js', () => ({
+  range: () => [1, 2, 3],
+}));
 
 describe('Pagination_02', () => {
   it('renders component', () => {
-    render(<Pagination total={50} limit={10} currentPage={1} />);
+    render(<Pagination total={30} limit={10} currentPage={1} />);
 
     const pageContainers = screen.getAllByTestId('page-container');
 
-    expect(pageContainers).toHaveLength(5);
+    expect(pageContainers).toHaveLength(3);
     expect(pageContainers[0]).toHaveTextContent('1');
   });
 
@@ -19,7 +24,7 @@ describe('Pagination_02', () => {
 
     render(
       <Pagination
-        total={50}
+        total={30}
         limit={10}
         currentPage={1}
         selectPage={handleClick}
@@ -32,5 +37,12 @@ describe('Pagination_02', () => {
 
     expect(handleClick).toHaveBeenCalledOnce();
     expect(handleClick).toHaveBeenCalledWith(1);
+  });
+
+  it('spies on utils', () => {
+    vi.spyOn(utils, 'range');
+    render(<Pagination total={40} limit={10} selectPage={1} />);
+
+    expect(utils.range).toHaveBeenCalledWith(1, 5);
   });
 });
