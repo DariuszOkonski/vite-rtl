c2bd6f17de04af5a20e4e7aef05acd35d6b8b281
Dariusz Okonski <darek200180@gmail.com>
2026-05-26 19:09:11 +0200

    add tests for Tags

---
+++ src/04_tags/Tags_04.test.jsx
@@ -0,0 +1,52 @@
+import { render, screen } from "@testing-library/react";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+import Tags from "./Tags";
+import axios from "axios";
+
+describe("Tags", () => {
+  const server = setupServer(
+    http.get("http://localhost:3004/tags", () => {
+      return HttpResponse.json([
+        { id: "1", name: "foo" },
+        { id: "2", name: "bar" },
+      ]);
+    }),
+  );
+
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+  afterEach(() => server.resetHandlers());
+
+  it("render Tags", async () => {
+    render(<Tags />);
+
+    const tags = await screen.findAllByTestId("tag");
+
+    expect(tags).toHaveLength(2);
+    expect(tags[0]).toHaveTextContent("foo");
+    expect(tags[1]).toHaveTextContent("bar");
+  });
+
+  it("spyOn axios", async () => {
+    const mockedResponse = {
+      data: [{ id: "1", name: "foo1" }],
+    };
+    vi.spyOn(axios, "get").mockResolvedValue(mockedResponse);
+    render(<Tags />);
+
+    const tags = await screen.findAllByTestId("tag");
+
+    expect(tags).toHaveLength(1);
+    expect(tags[0]).toHaveTextContent("foo1");
+  });
+});
