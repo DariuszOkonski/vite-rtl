7036519426f99fa59c2d7c9395f166d09f07ac3a
Dariusz Okoński <darek200180@gmail.com>
2026-05-28 08:19:01 +0200

    add tests for useFetch_01

--- src/06_useFetch/useFetch.jsx
+++ src/06_useFetch/useFetch.jsx
@@ -1,13 +1,13 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const useFetch = (url) => {
-  const baseUrl = "http://localhost:3004";
+  const baseUrl = 'http://localhost:3004';
   const [response, setResponse] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [options, setOptions] = useState({});
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   const doFetch = (options = {}) => {
     setOptions(options);
@@ -24,14 +24,14 @@ const useFetch = (url) => {
         const requestOptions = {
           ...options,
           headers: {
-            authorization: token ? `Token ${token}` : "",
+            authorization: token ? `Token ${token}` : '',
           },
         };
 
         const res = await axios.request(baseUrl + url, requestOptions);
         setResponse(res.data);
       } catch (err) {
-        setError(err);
+        setError(err.response.data);
       }
       setIsLoading(false);
     };
